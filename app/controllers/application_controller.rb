b741acfe17703632858d2092400e9c2005490a75
gkrishn0 <gkrishn0@chicagobooth.edu>
2021-04-21 21:53:19 +0000

    something isnt working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,26 @@
 class ApplicationController < ActionController::Base
-def add_form
-   render({:template => "math_templates/add_form.html.erb"})
+
+def street_to_coords_results
+  @user_inputs = params.fetch("user_street_address")
+  maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=Merchandise%20Mart%20Chicago&key=AIzaSyD8RrOFB0dPsF-leqeFJdmX3yOvcQbfNyY"
+  file = open(maps_url)
+  contents=file.read
+  @parsed_contents = JSON.parse(contents)
+  render({ :template => "math_templates/street_to_coords_results.html.erb"})
+  #@file = open ("https://maps.googleapis.com/maps/api/geocode/json?address=Merchandise%20Mart%20Chicago&key=AIzaSyD8RrOFB0dPsF-leqeFJdmX3yOvcQbfNyY")
+#https://maps.googleapis.com/maps/api/geocode/json?address=Merchandise%20Mart%20Chicago&key=AIzaSyD8RrOFB0dPsF-leqeFJdmX3yOvcQbfNyY
+
 end
 
-  def addition_results
+def addition_results
   #Parameters: {"first_num"=>"2", "second_num"=>"3"}
   @first = params.fetch("first_num").to_f
   @second = params.fetch("second_num").to_f
   @outcome = @first+@second
-  render({:template => "math_templates/add_results.html.erb"})
+  render({ :template => "math_templates/add_results.html.erb"})
+end
+def test
+   render({ :template => "math_templates/add_form.html.erb"})
+   #render({ :template => "math_templates/add_results.html.erb"})
 end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get("/add", {:controller => "application", :action=> "add_form"})
+  #get("/add", {:controller => "application", :action=> "add_form"})
   get("/wizard_add", {:controller => "application", :action=> "addition_results"})
+  get("/add", {:controller => "application", :action=> "test"})
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
