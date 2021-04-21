2cac721f21d4b84885aabb69cc5a0dfc984fac08
gkrishn0 <gkrishn0@chicagobooth.edu>
2021-04-21 20:44:00 +0000

    finished addition results

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
-def addition_results
+def add_form
+  
+end
+
+  def addition_results
+  #Parameters: {"first_num"=>"2", "second_num"=>"3"}
+  @first = params.fetch("first_num").to_f
+  @second = params.fetch("second_num").to_f
+  @outcome = @first+@second
   render({:template => "math_templates/add_results.html.erb"})
 end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get("/add", {:controller => "application", :action=> "add_form"})
   get("/wizard_add", {:controller => "application", :action=> "addition_results"})
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
